cbbf1871a88a6082afa72d09787b7f449d13101e
purna9101 <purnachandra8019@gmail.com>
2026-09-11 22:30:47 +0530

    Refactor application validation types

--- app/schemas.py
+++ app/schemas.py
@@ -1,31 +1,45 @@
-from pydantic import BaseModel, ConfigDict, field_validator
+from pydantic import BaseModel, ConfigDict, field_validator, BeforeValidator
+from enum import Enum
+from typing import Annotated
+
+
+class ApplicationStatus(Enum):
+    APPLIED=   "applied"
+    INTERVIEW= "interview"
+    OFFER=     "offer"
+    SELECTED=  "selected"
+    REJECTED=  "rejected"
+
+def normalize_status(value):
+    value= value.strip()
+    value= value.lower()
+    if not value:
+        raise ValueError("Invalid Status ")
+        
+    return value
+
+def normalize_text(value):
+    value= value.strip()
+    if not value:
+        raise ValueError("field cannot be empty")
+
+    return value
+
+Status = Annotated[
+    ApplicationStatus,
+    BeforeValidator(normalize_status)
+]
+
+NonEmptyString= Annotated[
+    str,
+    BeforeValidator(normalize_text)
+]
+
 
 class ApplicationCreate(BaseModel):
-    company: str
-    position: str
-    status: str
-
-    @field_validator("company", "position")
-    @classmethod
-    def validate_text_fields(cls, value):
-        value = value.strip()
-
-        if not value:
-            raise ValueError("field cannot be empty")
-
-        return value
-
-    @field_validator("status")
-    @classmethod
-    def validate_status(cls, value):
-        value= value.strip()
-        value= value.lower()
-        allowed_statuses= ["applied", "interview", "offer", "selected", "rejected"]
-        if not value:
-            raise ValueError("status cannot be empty")
-        if value not in allowed_statuses:
-            raise ValueError("invalid status")
-        return value
+    company: NonEmptyString
+    position: NonEmptyString
+    status: Status
 
 
 class ApplicationResponse(BaseModel):
@@ -40,6 +54,6 @@ class ApplicationDelete(BaseModel):
     message : str
 
 class ApplicationUpdate(BaseModel):
-    company : str | None = None
-    position: str | None = None
-    status  : str | None = None
+    company : NonEmptyString | None = None
+    position: NonEmptyString | None = None
+    status  : Status | None = None
